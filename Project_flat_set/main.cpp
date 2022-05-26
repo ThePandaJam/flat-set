e993a4b22b7c38bef9014bf434bb83d6e29e1b89
ThePandaJam <vekrav@gmail.com>
2022-05-26 16:39:29 +0100

    Initial Commit

---
+++ Project_flat_set/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project_flat_set
+//
+//  Created by Veronika Kravchenko on 26/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
